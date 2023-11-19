bbb448d7cfaac29ba4423ad86b5af7c7c5772628
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2023-11-19 10:55:20 -0300

    docs: add documentation to functions

--- randomized.c
+++ randomized.c
@@ -1,22 +1,27 @@
-
+// Struct to represent a point in [0,DOMAIN]x[0,DOMAIN].
 typedef struct {
     double x, y;
 } Point;
 
+// Struct to represent a node in a linked list where
+// points will be stored in the hash table.
 typedef struct Node {
     Point point;
     struct Node* next;
 } Node;
 
+// For defining hash functions int->int.
 typedef int (*IntFunction)(int);
 
+// Calculated the Euclidean distance between two points in R2.
 double calculateDistance(Point p1, Point p2) {
     double dx = p1.x - p2.x, dy = p1.y - p2.y;
     return sqrt(dx * dx + dy * dy);
 }
 
 // Calculates de d parameter used in the first part of the randomized algorithm.
-// This d will be used for grid dxd squares.
+// This d will be used for grid dxd cells.
+// It is calculated by picking n times pairs of points and returning the minimum distance.
 float calculateParameterD(Point *points, int n) {
 
 float current_min_d = 2; // 2 is greater than domain
@@ -39,23 +44,52 @@ float current_min_d = 2; // 2 is greater than domain
 }
 
 // The algorithm finds the closest pair of points and saves them in the array of size 2 closestPair.
+// It works by dividing the space in a grid of dxd quadrants,
+// creating a hash table to store all the points with quadrant's indices as keys,
+// and comparing each point to surrounding quadrants using this hash table.
+
+// For example:
+// The space is [0,DOMAIN]x[0,DOMAIN]
+// It is divided in quadrants of size dxd.
+// The resulting indexing for the quadrants could be
+// 0  1  2  3  4  5 
+// 6  7  8  9  10 11
+// 12 13 14 15 16 17
+// Doing idx=hash(15) will access the hash table at idx, 
+// where the linked list with all points in this quadrant is stored.
 void randomized(Point *points, int numPoints, Point *closestPair) {
 
+    // Calculate the parameter for the size of the quadrants.
     float d = calculateParameterD(points);
 
-    // Create the hash table with the points, indexed by the hash of the index of the cell they belong to.
+    // Create the hash table with the points, indexed by the hash of the index of the quadrant they belong to.
     Node* hashTable[m];
     createHashTable(points, hashTable, m, hashingFun, d);
 
-    // Compare each point with surrounding quadrants’ points searching for minimum.
+    // Compare each point with surrounding quadrants’ points, searching for minimum.
     comparePoints(points, hashTable, closestPair);
 
 }
 
+// Creates a hash table with linked lists of points.
+// The hash table is an array that contains pointers to Nodes (linked lists).
+// idx (int) | nptr (Node*), where *nptr = Node(p1, Node(p2, Node(p3, ..., Node(pz, NULL))))
+// For example:
+// 0 | nptr0, where *nptr0 = Node((x1,x1), Node((x2,y2), NULL))
+// 1 | nptr1, where *nptr1 = NULL
+// 2 | nptr2, where *nptr2 = Node((x3,x3), NULL)
+// 3 | nptr3, where *nptr3 = Node((x4,x4), Node((x5,y5), NULL))
+// ...
+// Receives the hash table (and its size) where the pointers will be stored.
+// Receives the array of points (and its size) which will be stored.
+// Receives the hash function used to find where to store a point.
+// Receives the parameter d to find the quadrant a point belongs to.
 void createHashTable(Point* points, Node* hashTable, int m, IntFunction hashingFun, float d) {
 
 }
 
+// Iterates over the array of points, finds their quadrant and its neighbors, 
+// compares the current point to all others in these quadrants, updating minimum.
 void comparePoints(Point* points, array* hashTable, Point *closestPair) {
 
 }
@@ -65,8 +99,8 @@ void comparePoints(Point* points, array* hashTable, Point *closestPair) {
 // 0  1  2  3  4  5 
 // 6  7  8  9  10 11
 // 12 13 14 15 16 17
-// They return the index in the hash table using the index of its quadrant as a key.
-
+// A key is a quadrant index, for example, key=15.
+// Doing idx=hash(15) will return where in the hash table this quadrant is stored.
 int universalHashFun(int key) {
 
 }
