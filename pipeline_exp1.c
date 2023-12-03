63c30febfec83ee0224746b89a0cbeeb5e7b6fa7
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2023-12-03 14:43:42 -0300

    feat: add three pipelines for experiments

---
+++ pipeline_exp1.c
@@ -0,0 +1,137 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+
+#include "sweep-line.c"
+#include "randomized.c"
+
+// Values for amount of points.
+#define N_ONE 5000000
+#define N_TWO 25000000
+#define N_THREE 50000000
+
+// Amount of times the algorithm will be executed generate the histogram.
+#define NUMBER_REPS 101
+
+double randomZeroToOne() {
+    return (double)rand() / (RAND_MAX + 1.0);
+}
+
+Point generateRandomPoint() {
+    Point randomPoint;
+    randomPoint.x = randomZeroToOne();
+    randomPoint.y = randomZeroToOne();
+    return randomPoint;
+}
+
+int main() {
+    // Set the seed for testing and evaluation purposes.
+    // This seed will allow for different d values in randomized in each repetition.
+    srand(123);
+
+    printf("============================================\n");
+    printf("EXP. 1: SAME ARRAY FOR GIVEN SIZE...\n");
+    printf("============================================\n");
+
+    // This file will be used to plot the performance data.
+    FILE *results_file = fopen("performance_results_1.txt", "w");
+
+    // Iterate over different sizes of arrays.
+    int n_sizes[3];
+    n_sizes[0] = N_ONE;
+    n_sizes[1] = N_TWO;
+    n_sizes[2] = N_THREE;
+
+    for(int i=0; i<3; i++) {
+
+        int n = n_sizes[i];
+
+        printf("--------------------------------------------\n");
+        printf("Finding the closest pair from %d points for %d repetitions...\n", n, NUMBER_REPS);
+        printf("--------------------------------------------\n");
+
+        printf("- Creating array...\n");
+
+        // Generate array of n points.
+        Point *points = (Point *)malloc(n * sizeof(Point));
+        if (points == NULL) {
+            fprintf(stderr, "Memory allocation failed.\n");
+            return 1;
+        }
+
+        // Fill the array with random points in the domain [0,1)x[0,1).
+        for (int i = 0; i < n; i++) {
+            points[i] = generateRandomPoint();
+            //printf("Point: (%f, %f)\n", points[i].x, points[i].y);
+        }
+
+        // Execute both algorithms with same array NUMBER_REPS times.
+        for (int rep = 1; rep < NUMBER_REPS + 1; rep++) {
+
+            printf("Repetition number %d...\n", rep);
+
+            //printf("- Finding closest pair with sweep-line...\n");
+            clock_t start_time_sl = clock();
+            Point *closestPair_sl = (Point *)malloc(2 * sizeof(Point));
+            sweepline(points, n, closestPair_sl);
+            clock_t end_time_sl = clock();
+            double cpu_time_used_sl = ((double)(end_time_sl - start_time_sl)) / CLOCKS_PER_SEC;
+            double closestDistance_sl =  calculateDistance(closestPair_sl[0],closestPair_sl[1]);
+            printf("- Closest pair with sweep-line: (%.*lf, %.*lf) and (%.*lf, %.*lf) with distance %.*lf\n",
+                   DBL_DIG, closestPair_sl[0].x, DBL_DIG, closestPair_sl[0].y,
+                   DBL_DIG, closestPair_sl[1].x, DBL_DIG, closestPair_sl[1].y,
+                   DBL_DIG, closestDistance_sl);
+            free(closestPair_sl);
+
+            //printf("- Finding closest pair with randomized universal...\n");
+            clock_t start_time_rdu = clock();
+            Point *closestPair_rdu = (Point *)malloc(2 * sizeof(Point));
+            randomized(points, n, universalHashFun, closestPair_rdu);
+            clock_t end_time_rdu = clock();
+            double cpu_time_used_rdu = ((double)(end_time_rdu - start_time_rdu)) / CLOCKS_PER_SEC;
+            double closestDistance_rdu =  calculateDistance(closestPair_rdu[0],closestPair_rdu[1]);
+            printf("- Closest pair with randomized universal: (%.*lf, %.*lf) and (%.*lf, %.*lf) with distance %.*lf\n",
+                   DBL_DIG, closestPair_rdu[0].x, DBL_DIG, closestPair_rdu[0].y,
+                   DBL_DIG, closestPair_rdu[1].x, DBL_DIG, closestPair_rdu[1].y,
+                   DBL_DIG, closestDistance_rdu);
+            free(closestPair_rdu);
+
+            //printf("- Finding closest pair with randomized faster...\n");
+            clock_t start_time_rdf = clock();
+            Point *closestPair_rdf = (Point *)malloc(2 * sizeof(Point));
+            randomized(points, n, fasterHashFun, closestPair_rdf);
+            clock_t end_time_rdf = clock();
+            double cpu_time_used_rdf = ((double)(end_time_rdf - start_time_rdf)) / CLOCKS_PER_SEC;
+            double closestDistance_rdf =  calculateDistance(closestPair_rdf[0],closestPair_rdf[1]);
+            printf("- Closest pair with randomized faster: (%.*lf, %.*lf) and (%.*lf, %.*lf) with distance %.*lf\n",
+                   DBL_DIG, closestPair_rdf[0].x, DBL_DIG, closestPair_rdf[0].y,
+                   DBL_DIG, closestPair_rdf[1].x, DBL_DIG, closestPair_rdf[1].y,
+                   DBL_DIG, closestDistance_rdf);
+            free(closestPair_rdf);
+
+            //printf("- Finding closest pair with randomized mersenne...\n");
+            clock_t start_time_rdm = clock();
+            Point *closestPair_rdm = (Point *)malloc(2 * sizeof(Point));
+            randomized(points, n, mersenneHashFun, closestPair_rdm);
+            clock_t end_time_rdm = clock();
+            double cpu_time_used_rdm = ((double)(end_time_rdm - start_time_rdm)) / CLOCKS_PER_SEC;
+            double closestDistance_rdm =  calculateDistance(closestPair_rdm[0],closestPair_rdm[1]);
+            printf("- Closest pair with randomized mersenne: (%.*lf, %.*lf) and (%.*lf, %.*lf) with distance %.*lf\n",
+                   DBL_DIG, closestPair_rdm[0].x, DBL_DIG, closestPair_rdm[0].y,
+                   DBL_DIG, closestPair_rdm[1].x, DBL_DIG, closestPair_rdm[1].y,
+                   DBL_DIG, closestDistance_rdm);
+            free(closestPair_rdm);
+
+            fprintf(results_file, "%d, %d, %lf, %lf, %lf, %lf\n", n, rep, cpu_time_used_sl, cpu_time_used_rdu, cpu_time_used_rdf, cpu_time_used_rdm);
+                
+        }
+
+        free(points);
+
+    }
+    
+    fclose(results_file);
+    return 0;
+}
--- plot_results.py
+++ plot_results.py
@@ -1,13 +1,60 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+"""
+# ============ Experiment 1 ============
+# Creates histograms to see how the cpu times are distributed, for both algorithms,
+# for a selection of 3 array sizes.
 
+print("============ Experiment 1 ============")
 file_path = 'performance_results_1.txt'
-column_names = ['n', 'rep', 'cpu_sl', 'cpu_rd']
+column_names = ['n', 'rep', 'cpu_sl', 'cpu_rdu', 'cpu_rdf', 'cpu_rdm']
+raw_df = pd.read_csv(file_path, names=column_names, delimiter=',')
+exp1_df = raw_df
+
+fixed_n_array = [5000000, 25000000, 50000000]
+letters = ['a', 'b', 'c']
+
+for i in [0,1,2]:
+    fixed_n = fixed_n_array[i]
+    letter = letters[i]
+
+    # Filter the DataFrame for the fixed value of n
+    filtered_df = exp1_df[exp1_df['n'] == fixed_n]
+
+    # Set the number of bins for the histogram
+    num_bins = 10
+
+    # Plot histograms for avg_cpu_sl
+    plt.figure(figsize=(10, 6))
+    plt.hist(filtered_df['cpu_sl'], bins=num_bins, alpha=0.5, label='cpu_sl')
+    plt.xlabel('time (s)')
+    plt.ylabel('frequency')
+    plt.title(f'Histogram for n={fixed_n}')
+    plt.legend()
+    plt.savefig('exp1'+letter+'a.png')
+    plt.show()
+
+    # Plot histograms for avg_cpu_rd
+    plt.figure(figsize=(10, 6))
+    plt.hist([filtered_df['cpu_sl'], filtered_df['cpu_ru'], filtered_df['cpu_rdf'], filtered_df['cpu_rdm']], alpha=0.7, label=['cpu_sl', 'cpu_rdu', 'cpu_rdf', 'cpu_rdm'])
+    plt.xlabel('time (s)')
+    plt.ylabel('frequency')
+    plt.title(f'Histogram for n={fixed_n}')
+    plt.legend()
+    plt.savefig('exp1'+letter+'b.png')
+    plt.show()
+
+# ============ Experiment 2 ============
+# Plots the average time taken by each algorithm as a function of the number of points.
+
+print("============ Experiment 2 ============")
+file_path = 'performance_results_2.txt'
+column_names = ['n', 'rep', 'cpu_sl', 'cpu_rdu', 'cpu_rdf', 'cpu_rdm']
 raw_df = pd.read_csv(file_path, names=column_names, delimiter=',')
 
 # Specify the chunk size
-NUMBER_REPS = 101
+NUMBER_REPS = 5
 
 # Create an empty DataFrame to store the results
 result_df = pd.DataFrame(columns=['chunk', 'n', 'avg_cpu_sl', 'avg_cpu_rd'])
@@ -19,73 +66,24 @@ for chunk_number, chunk_df in enumerate(pd.read_csv(file_path, names=column_name
     # Calculate the average for the cpu time columns
     n = chunk_df['n'][0]
     avg_cpu_sl = chunk_df['cpu_sl'].mean()
-    avg_cpu_rd = chunk_df['cpu_rd'].mean()
+    avg_cpu_rdu = chunk_df['cpu_rdu'].mean()
+    avg_cpu_rdf = chunk_df['cpu_rdf'].mean()
+    avg_cpu_rdm = chunk_df['cpu_rdm'].mean()
 
     # Append the results to the result DataFrame
-    result_df = result_df._append({'chunk': chunk_number + 1, 'n': n, 'avg_cpu_sl': avg_cpu_sl, 'avg_cpu_rd': avg_cpu_rd}, ignore_index=True)
+    result_df = result_df._append({'chunk': chunk_number + 1, 'n': n, 'avg_cpu_sl': avg_cpu_sl, 'avg_cpu_rdu': avg_cpu_rdu, 'avg_cpu_rdf': avg_cpu_rdf, 'avg_cpu_rdm': avg_cpu_rdm}, ignore_index=True)
 
-# Display the result DataFrame
-print(result_df)
-
-# ============ Experiment 1 ============
-# Creates histograms to see how the cpu times are distributed, for both algorithms.
-print("============ Experiment 1 ============")
-exp1_df = raw_df
-
-fixed_n = 50000000
-
-# Filter the DataFrame for the fixed value of n
-filtered_df = exp1_df[exp1_df['n'] == fixed_n]
-
-# Set the number of bins for the histogram
-num_bins = 10
-
-# Plot histograms for avg_cpu_sl
-plt.figure(figsize=(10, 6))
-
-min_value = filtered_df['cpu_sl'].min()
-max_value =filtered_df['cpu_sl'].max()
-
-plt.hist(filtered_df['cpu_sl'], bins=num_bins, alpha=0.5, label='cpu_sl')
-plt.xlim(min_value, max_value)
-
-plt.xlabel('time (s)')
-plt.ylabel('frequency')
-plt.title(f'Histogram for n={fixed_n}')
-plt.legend()
-plt.savefig('exp1-sl.png')
-plt.show()
-
-# Plot histograms for avg_cpu_rd
-plt.figure(figsize=(10, 6))
-
-min_value = filtered_df['cpu_rd'].min()
-max_value =filtered_df['cpu_rd'].max()
-
-plt.hist(filtered_df['cpu_rd'], bins=num_bins, alpha=0.5, label='cpu_rd')
-plt.xlim(min_value, max_value)
-
-plt.xlabel('time (s)')
-plt.ylabel('frequency')
-plt.title(f'Histogram for n={fixed_n}')
-plt.legend()
-plt.savefig('exp1-rd.png')
-plt.show()
-
-# ============ Experiment 2 ============
-# Plots the average time taken by each algorithm as a function of the number of points.
-print("============ Experiment 2 ============")
 exp2_df = result_df
 print(exp2_df)
 
 # Plotting
 plt.figure(figsize=(10, 6))
 
-# Plot the u column as a function of the radix sort time column
-plt.plot(exp2_df['n'], exp2_df['avg_cpu_sl'], label='sweep-line')
-
-# Plot the u column as a function of the quicksort time column
-plt.plot(exp2_df['n'], exp2_df['avg_cpu_rd'], label='randomized')
+# Plot the time columns as a function of the size column
+plt.plot(exp2_df['n'], exp2_df['avg_cpu_sl'], label='sweep line')
+plt.plot(exp2_df['n'], exp2_df['avg_cpu_rdu'], label='randomized universal')
+plt.plot(exp2_df['n'], exp2_df['avg_cpu_rdf'], label='randomized faster')
+plt.plot(exp2_df['n'], exp2_df['avg_cpu_rdm'], label='randomized mersenne')
 
 # Set labels and title
 plt.xlabel('n')
@@ -98,24 +96,67 @@ plt.legend()
 plt.savefig('exp2.png')
 plt.show()
 
+"""
 # ============ Experiment 3 ============
 # Plots histogram of times taken by algorithms for 
 # different arrays of points of a fixed size.
-file_path = 'performance_results_2.txt'
-column_names = ['rep', 'cpu_sl', 'cpu_rd']
-df = pd.read_csv(file_path, names=column_names, delimiter=',')
 
+print("============ Experiment 3 ============")
+file_path = 'performance_results_3.txt'
+column_names = ['rep', 'cpu_sl', 'cpu_rdu', 'cpu_rdf', 'cpu_rdm']
+df = pd.read_csv(file_path, names=column_names, delimiter=',')
+exp3_df = df
+print(exp3_df)
+
+# ------- Plotting all in the same plot -------
 # Plot histogram with different colors for each column
-plt.hist([df['cpu_sl'], df['cpu_rd']], color=['blue', 'green'], alpha=0.7, label=['cpu_sl', 'cpu_rd'])
+plt.hist([exp3_df['cpu_sl'], exp3_df['cpu_rdu'], exp3_df['cpu_rdf'], exp3_df['cpu_rdm']], alpha=0.7, label=['cpu_sl', 'cpu_rdu', 'cpu_rdf', 'cpu_rdm'])
 
 # Add labels and title
 plt.xlabel('time (s)')
 plt.ylabel('frequency')
-plt.title('Histogram of cpu_sl and cpu_rd')
+plt.title('Histogram of cpu times')
 
 # Add legend
 plt.legend()
 
 # Show the plot
-plt.savefig('exp3.png')
+plt.savefig('exp3a.png')
+plt.show()
+
+# ------- Plotting each in their own plot -------
+# Set the number of bins for the histogram
+num_bins = 10
+fixed_n = 5000000
+
+plt.hist(exp3_df['cpu_sl'], bins=num_bins, alpha=0.5, label='cpu_sl')
+plt.xlabel('time (s)')
+plt.ylabel('frequency')
+plt.title(f'Histogram for sweep line for n={fixed_n}')
+plt.legend()
+plt.savefig('exp3b.png')
+plt.show()
+
+plt.hist(exp3_df['cpu_rdu'], bins=num_bins, alpha=0.5, label='cpu_rdu')
+plt.xlabel('time (s)')
+plt.ylabel('frequency')
+plt.title(f'Histogram for randomized universal for n={fixed_n}')
+plt.legend()
+plt.savefig('exp3b.png')
+plt.show()
+
+plt.hist(exp3_df['cpu_rdf'], bins=num_bins, alpha=0.5, label='cpu_rdf')
+plt.xlabel('time (s)')
+plt.ylabel('frequency')
+plt.title(f'Histogram for randomized faster for n={fixed_n}')
+plt.legend()
+plt.savefig('exp3c.png')
+plt.show()
+
+plt.hist(exp3_df['cpu_rdm'], bins=num_bins, alpha=0.5, label='cpu_rdm')
+plt.xlabel('time (s)')
+plt.ylabel('frequency')
+plt.title(f'Histogram for randomized mersenne for n={fixed_n}')
+plt.legend()
+plt.savefig('exp3d.png')
 plt.show()
